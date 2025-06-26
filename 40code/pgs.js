72921fcd783c54bc0db6a181aaedd208d502d749
YXY317Coder <YXY317Coder@qq.com>
2025-06-26 11:41:09 +0800

    Update pgs.js

--- 40code/pgs.js
+++ 40code/pgs.js
@@ -1,4 +1,8 @@
 //posts and gets
+res.header("Access-Control-Allow-Origin", "*"); // 或者指定特定的域名
+res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+res.header("Access-Control-Allow-Headers", "Content-Type");
+//post
 function posts(url, data) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
@@ -11,7 +15,7 @@ function posts(url, data) {
         return null;
     }
 }
-
+//get
 function gets(url) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, false);
