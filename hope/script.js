79c8376c644cf348b77ec59a70bb3d3a9ebad26b
YXY317Coder <YXY317Coder@qq.com>
2026-02-24 00:20:35 +0800

    Create script.js

---
+++ hope/script.js
@@ -0,0 +1,19 @@
+function home(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/";
+}
+function call(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/call/";
+}
+function talk(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/talk/";
+}
+function rule(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/rule/";
+}
+function about(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/about/";
+}
+function pay(){
+    window.location.href = "https://yxy317coder.github.io/others/hope/pay/";
+}
+//more code...
